9200eedcb1523c59e875f1e629166138e2829173
wsdwasdwawdw <jasonkarlrosales@gmail.com>
2026-07-24 14:20:11 +0800

    created folder for the scripts

---
+++ Scripts/online.js
@@ -0,0 +1,7 @@
+(function() {
+    const onlineButton = document.querySelector('.online');
+
+    onlineButton.addEventListener('click', () => {
+        alert("Online mode is currently under development. Please check back later for updates!");
+    });
+})();
